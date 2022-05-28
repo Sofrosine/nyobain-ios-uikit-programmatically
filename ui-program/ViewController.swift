b75f92a0f6e2be1773f3706711ca7f6d023c632c
Sofrosine <soultanmuh@gmail.com>
2022-05-28 11:45:05 +0700

    Initial Commit

---
+++ ui-program/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ui-program
+//
+//  Created by Soultan Muhammad Albar on 28/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
