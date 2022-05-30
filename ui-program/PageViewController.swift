dff489b2651cc3c810bdc299b3c9466a7c672703
Sofrosine <soultanmuh@gmail.com>
2022-05-30 14:25:21 +0700

    add bottom button and uicontrolpage

--- ui-program/PageViewController.swift
+++ ui-program/PageViewController.swift
@@ -10,14 +10,62 @@ import UIKit
 class PageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
     
     var pages = [UIViewController]()
-    let pageControl = UIPageControl()
+    let initialPage = 0
+    lazy var pageControl: UIPageControl = {
+        let pageControlView = UIPageControl()
+        pageControlView.translatesAutoresizingMaskIntoConstraints = false
+        pageControlView.frame = CGRect(x: 0, y: 0, width: 0, height: 80)
+        pageControlView.currentPageIndicatorTintColor = UIColor.orange
+        pageControlView.pageIndicatorTintColor = UIColor.lightGray
+        pageControlView.numberOfPages = self.pages.count
+        pageControlView.currentPage = initialPage
+        return pageControlView
+    }()
+    
+    lazy var btnNext: UIButton = {
+        var btnConfiguration = UIButton.Configuration.filled()
+      
+        btnConfiguration.title = "BERIKUTNYA"
+        btnConfiguration.attributedTitle?.font = UIFont(name: "CircularStd-Bold", size: 12)
+        btnConfiguration.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 32, bottom: 12, trailing: 32)
+        btnConfiguration.baseForegroundColor = .white
+        btnConfiguration.baseBackgroundColor = .orange
+        btnConfiguration.cornerStyle = .capsule
+        
+        let btn = UIButton(configuration: btnConfiguration, primaryAction: nil)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        
+        btn.addTarget(self, action: #selector(handleNextButton), for: .touchUpInside)
 
+        return btn
+    }()
+    
+    lazy var bottomStack: UIStackView = {
+        let bottomStackView = UIStackView(arrangedSubviews: [pageControl, btnNext])
+        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomStackView.axis = .horizontal
+        bottomStackView.distribution = .equalCentering
+        bottomStackView.sizeToFit()
+        bottomStackView.layoutIfNeeded()
+        
+        return bottomStackView
+    }()
+    
     override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
         super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func handleNextButton() {
+        if pageControl.currentPage < pages.count-1 {
+            pageControl.currentPage += 1
+        } else {
+            pageControl.currentPage = 0
+        }
+        setViewControllers([pages[pageControl.currentPage]], direction: .forward, animated: true)
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
@@ -64,7 +112,7 @@ class PageViewController: UIPageViewController, UIPageViewControllerDataSource, 
         // Do any additional setup after loading the view.
         self.dataSource = self
         self.delegate = self
-        let initialPage = 0
+        
         let page1 = ViewController(imageBoardName: "on_boarding1", titleBoard: "Belanja Murah", subtitleBoard: "Belanja sepuasnya gak perlu khawatir dompet ketar-ketir")
         let page2 = ViewController(imageBoardName: "on_boarding2", titleBoard: "Belanja Mudah", subtitleBoard: "Belanja semua kebutuhan harian dalam satu aplikasi")
         let page3 = ViewController(imageBoardName: "on_boarding3", titleBoard: "Belanja dari Rumah", subtitleBoard: "Belanja sambil santai, pesanan langsung diantar ke tempatmu. Bebas Ongkir!")
@@ -75,19 +123,12 @@ class PageViewController: UIPageViewController, UIPageViewControllerDataSource, 
         self.pages.append(page3)
         setViewControllers([pages[initialPage]], direction: .forward, animated: true, completion: nil)
         
-        // pageControl
-        self.pageControl.frame = CGRect()
-        self.pageControl.currentPageIndicatorTintColor = UIColor.black
-        self.pageControl.pageIndicatorTintColor = UIColor.lightGray
-        self.pageControl.numberOfPages = self.pages.count
-        self.pageControl.currentPage = initialPage
-        self.view.addSubview(self.pageControl)
+        view.addSubview(bottomStack)
         
-        self.pageControl.translatesAutoresizingMaskIntoConstraints = false
-        self.pageControl.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -40).isActive = true
-        self.pageControl.widthAnchor.constraint(equalTo: self.view.widthAnchor, constant: -20).isActive = true
-        self.pageControl.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        self.pageControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        bottomStack.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        bottomStack.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        bottomStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        
     }
     
     
